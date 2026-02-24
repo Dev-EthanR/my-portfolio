df70bccf911b40fcfd25d6b72a5102a697e3ce85
Ethan <ethanlegend7@gmail.com>
2026-02-24 13:39:52 +1100

    add tech stack

--- app/components/Badge.tsx
+++ app/components/Badge.tsx
@@ -9,13 +9,13 @@ const Badge = ({ imageSrc, title }: Props) => {
   return (
     <div className="relative ">
       <Image
-        className="peer"
+        className="peer rounded-md"
         src={imageSrc}
         alt={title}
         width={50}
         height={50}
       />
-      <div className="absolute -top-12 left-1/2 -translate-x-1/2 hidden peer-hover:block text-white bg-surface w-fit p-2 rounded-2xl">
+      <div className="absolute -top-12 left-1/2 -translate-x-1/2 hidden peer-hover:block text-white font-medium bg-surface w-fit p-2 rounded-2xl">
         {title}
         <div className="absolute left-1/2 -translate-x-1/2 w-0 h-0 border-l-25 border-l-transparent border-r-25 border-r-transparent border-t-20 border-surface"></div>
       </div>
--- app/components/TechStack.tsx
+++ app/components/TechStack.tsx
@@ -2,12 +2,28 @@ import React from "react";
 import Badge from "./Badge";
 
 const TechStack = () => {
+  const stack = [
+    { title: "JavaScript", src: "/badges/javascript.webp" },
+    { title: "TypeScript", src: "/badges/typescript.png" },
+    { title: "React", src: "/badges/react.svg" },
+    { title: "Next.js", src: "/badges/nextjs.webp" },
+    { title: "HTML", src: "/badges/html.png" },
+    { title: "CSS", src: "/badges/css.png" },
+    { title: "Tailwind", src: "/badges/tailwind.png" },
+    { title: "SASS", src: "/badges/sass.png" },
+    { title: "Bootstrap", src: "/badges/bootstrap.png" },
+    { title: "Prisma", src: "/badges/prisma.jpg" },
+    { title: "Git", src: "/badges/git.svg" },
+    { title: "Vercel", src: "/badges/vercel.svg" },
+  ];
+
   return (
     <div className="flex flex-col items-center">
       <h2 className="text-2xl font-bold text-center mb-4">My Tech Stack</h2>
       <div className="grid grid-cols-5 gap-5">
-        <Badge title="JavaScript" imageSrc="/badges/javascript.webp" />
-        <Badge title="JavaScript" imageSrc="/badges/javascript.webp" />
+        {stack.map((tech) => (
+          <Badge key={tech.title} title={tech.title} imageSrc={tech.src} />
+        ))}
       </div>
     </div>
   );
