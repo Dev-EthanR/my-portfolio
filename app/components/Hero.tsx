33d25370d86da68b6590f38a53a8eda6e36c9266
Ethan <ethanlegend7@gmail.com>
2026-02-25 14:47:44 +1100

    add header to hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,13 +1,13 @@
 const Hero = () => {
   return (
-    <header className="flex flex-col items-center">
+    <header className="flex flex-col items-center  max-w-160 mx-auto">
       <h1 className="text-5xl font-semibold mb-4">Ethan Rechichi</h1>
-      <h2 className="text-2xl font-light border-b border-white/25 w-fit mb-4 pb-1.5 max-w-160 text-center">
+      <h2 className="text-2xl font-light border-b border-white/25 w-fit mb-4 pb-1.5 text-center">
         Full Stack Developer
       </h2>
       <h3 className="mb-3 ">📍 Melbourne, Australia 🇦🇺</h3>
       <p className="text-sm font-light text-gray-300 border-b-accent border-b pb-2">
-        opening line goes here... (1 sentence)
+        Striving to be the improve each day
       </p>
     </header>
   );
