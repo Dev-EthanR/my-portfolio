f2bdd4cf250a7bb8a4ac4ad77ee5e1cae4a6e9b1
Ethan <ethanlegend7@gmail.com>
2026-02-25 16:48:54 +1100

    handle no future improvements

--- app/projects/[project]/components/ProjectDetails/Features.tsx
+++ app/projects/[project]/components/ProjectDetails/Features.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const Features = ({ data }: Props) => {
   return (
-    <ul className="border-r py border-accent pl-8 mr-3">
+    <ul>
       <h3 className="text-xl font-medium mb-2">Features:</h3>
       {data.features.map((feature) => (
         <li key={feature} className="list-disc max-w-90">
--- app/projects/[project]/components/ProjectDetails/FutureImprovements.tsx
+++ app/projects/[project]/components/ProjectDetails/FutureImprovements.tsx
@@ -4,8 +4,9 @@ interface Props {
   data: Project;
 }
 const FutureImprovements = ({ data }: Props) => {
+  if (data.futureImprovements.length === 0) return null;
   return (
-    <ul>
+    <ul className="border-l border-accent pl-8">
       <h3 className="text-xl font-medium mb-2 ">Future Improvments:</h3>
       {data.futureImprovements.map((future) => (
         <li key={future} className="list-disc max-w-90">
