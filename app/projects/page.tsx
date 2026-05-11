58a35844ee2bd90b7e89cffd560996b015a49a8c
Ethan <46520558+Dev-EthanR@users.noreply.github.com>
2026-05-11 14:43:31 +1000

    further layout adjustments

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -17,7 +17,7 @@ const page = async ({ searchParams }: { searchParams: { tech?: string } }) => {
         Projects
       </h1>
       <ProjectFilter data={jsonData} />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 ">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 ">
         {filteredData.map((data) => (
           <ProjectCard
             key={data.id}
