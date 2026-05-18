91cbab170252a1d0f970faa02a4515b08fae2bf5
Ethan <46520558+Dev-EthanR@users.noreply.github.com>
2026-05-18 13:48:11 +1000

    update job data

--- data/journey.ts
+++ data/journey.ts
@@ -7,7 +7,7 @@ interface Journey {
 export const journey: Journey[] = [
   {
     date: "2026 - PRESENT",
-    title: "Software Developer | Interpack Pty Ltd",
+    title: "Software Developer Intern | Interpack Pty Ltd",
     description: "File check Automation to replace manual checking",
   },
   {
